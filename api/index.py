3d9bc78f3a7974ef46bcf583e186bd336e3d6b1c
Weijing Jay Lin <weijingjaylin@gmail.com>
2023-08-22 23:46:17 -0700

    test env

--- api/index.py
+++ api/index.py
@@ -1,7 +1,38 @@
+import os, openai, langchain, pinecone
+
 from fastapi import FastAPI
+from langchain.document_loaders import DirectoryLoader, TextLoader,UnstructuredPDFLoader, OnlinePDFLoader, PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Pinecone
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader, PyPDFLoader
+from dotenv import load_dotenv
+
+print("loading files")
+text_splitter = RecursiveCharacterTextSplitter(
+    # Set a really small chunk size, just to show.
+    chunk_size = 2000,
+    chunk_overlap  = 0,
+    length_function = len,
+)
+
+loader = DirectoryLoader('./data', glob="./prod/*.pdf", loader_cls=PyPDFLoader)
+documents = loader.load()
+
+print("splitting files")
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+texts = text_splitter.split_documents(documents)
+print("done splitting files: ", len(texts), "texts")
 
 app = FastAPI()
 
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+
+@app.get("/api/env")
+def env():
+    return {"message": os.getenv("PINECONE_API_KEY")}
